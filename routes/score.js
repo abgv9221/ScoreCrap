20200fc7da33014cc54bfb8ba1ae09720b39b181
ghondar <abgv9221@gmail.com>
2015-05-31 13:42:22 -0500

    support promises

--- routes/score.js
+++ routes/score.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var scrap = require('scrap');
+var scrap = require('../lib/promise').get;
 var URL = 'http://depor.pe/';
 
 router.get('/', function(req, res, next){
-	scrap(URL, function(err, $){
-		res.send($.html());
-	});
+	scrap(URL)
+		.then(function($){
+			res.send($.html());
+		});
 });
 
 module.exports = router;
