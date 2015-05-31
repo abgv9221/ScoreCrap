d7c91ce1398db40f031740ee578af8582a596514
ghondar <abgv9221@gmail.com>
2015-05-31 15:59:36 -0500

    avances

--- routes/score.js
+++ routes/score.js
@@ -1,12 +1,34 @@
 var express = require('express');
 var router = express.Router();
 var scrap = require('../lib/promise').get;
-var URL = 'http://depor.pe/';
+var URL = 'http://www.resultados-futbol.com/';
 
 router.get('/', function(req, res, next){
 	scrap(URL)
 		.then(function($){
-			res.send($.html());
+			var a = []
+			$("div.liga>table>tbody>tr").each(function(i, element){
+				var goles = $(this).find('a>div.clase').text().split('-');
+				var json = {
+					fecha: $(this).html(),
+					hora: $(this).find('span.fecha-status').html(),
+					estado: $(this).find('span.fecha-status.live').html(),
+					equipol: {
+						nombre: $(this).find('span.padr>a').html(),
+						imagen: $(this).find('span.padr>img').attr('src'),
+						goles: goles[0]
+					},
+					equipor: {
+						nombre: $(this).find('span.padl>a').html(),
+						imagen: $(this).find('span.padl>img').attr('src'),
+						goles: goles[1]
+					}
+				}
+				if(json.hora){
+					a.push(json);
+				}		
+			});
+			res.send(a)
 		});
 });
 
