6f8a8095d7992f078953a161ee6ed521dbd8a4e8
ghondar <abgv9221@gmail.com>
2015-05-31 13:27:26 -0500

    change module

--- routes/score.js
+++ routes/score.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var scrap = require('scrap');
+var URL = 'http://depor.pe/';
 
 router.get('/', function(req, res, next){
-
+	scrap(URL, function(err, $){
+		res.send($.html());
+	});
 });
 
 module.exports = router;
