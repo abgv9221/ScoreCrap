9055595c2242af16a629571c1f067396cde1de58
Anthony Gavilan Vinces <abgv9221@gmail.com>
2015-06-03 23:04:42 -0500

    score de ligas finalizado

--- routes/score.js
+++ routes/score.js
@@ -7,28 +7,41 @@ router.get('/', function(req, res, next){
 	scrap(URL)
 		.then(function($){
 			var a = []
-			$("div.liga>table>tbody>tr").each(function(i, element){
-				var goles = $(this).find('a>div.clase').text().split('-');
-				var json = {
-					fecha: $(this).find('td:first-child').text().substring(0,10),
-					hora: $(this).find('span.fecha-status').html(),
-					estado: $(this).find('span.fecha-status.live').html(),
-					equipol: {
-						nombre: $(this).find('span.padr>a').html(),
-						imagen: $(this).find('span.padr>img').attr('src'),
-						goles: goles[0]
-					},
-					equipor: {
-						nombre: $(this).find('span.padl>a').html(),
-						imagen: $(this).find('span.padl>img').attr('src'),
-						goles: goles[1]
-					}
-				}
-				if(json.hora){
+			$("div.listaligas").each(function(i, element){
+				$(this).find('div.liga').each(function(i, element){
+					var json = {};
+					var partidos = [];
+					json.liga = $(this).find('div.title>a').text();
+					$(this).find("table>tbody>tr").each(function(i, element){
+						var datos = $(this).find('a>div.clase').text().split('-');
+						var postergado;
+						var goles = [];
+						(datos[0].search(':') != -1)?postergado = datos[0]:goles = datos;
+						var temporal = {
+							fecha: $(this).find('td:first-child').text().substring(0,10),
+							hora: $(this).find('span.fecha-status').html(),
+							postergado: postergado,
+							estado: $(this).find('span.fecha-status.live').html(),
+							equipol: {
+								nombre: $(this).find('span.padr>a').html(),
+								imagen: $(this).find('span.padr>img').attr('src'),
+								goles: goles[0]
+							},
+							equipor: {
+								nombre: $(this).find('span.padl>a').html(),
+								imagen: $(this).find('span.padl>img').attr('src'),
+								goles: goles[1]
+							}
+						}
+						if(temporal.hora){
+							partidos.push(temporal);
+						}		
+					});
+					json.partidos = partidos;
 					a.push(json);
-				}		
+				});
 			});
-			res.send(a)
+			res.send(a);
 		});
 });
 
